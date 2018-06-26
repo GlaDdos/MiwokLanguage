795bacc20aa4d55c4dff95e0d0372c66710d1ff0
GlaDdos <kamil.kawa@gmail.com>
2018-06-26 18:54:17 +0200

    Views for each category and events listeners to launch them

--- app/src/main/java/com/example/kamil/miwoklanguage/MainActivity.java
+++ app/src/main/java/com/example/kamil/miwoklanguage/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.kamil.miwoklanguage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewColors;
+    TextView textViewFamily;
+    TextView textViewNumbers;
+    TextView textViewPhrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewColors = (TextView) findViewById(R.id.colors);
+        textViewFamily = (TextView) findViewById(R.id.family);
+        textViewNumbers = (TextView) findViewById(R.id.numbers);
+        textViewPhrases = (TextView) findViewById(R.id.phrases);
+
+        textViewColors.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        textViewFamily.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textViewNumbers.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textViewPhrases.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void openNumbersActivity(View view) {
+
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
     }
 }
