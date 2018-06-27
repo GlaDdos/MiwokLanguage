733128ab2e7b75cf99f526c6bda22b3a7531e6ea
GlaDdos <kamil.kawa@gmail.com>
2018-06-27 12:40:41 +0200

    modified word adapter to show images

--- app/src/main/java/com/example/kamil/miwoklanguage/WordAdapter.java
+++ app/src/main/java/com/example/kamil/miwoklanguage/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,9 +34,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView primaryItem = (TextView) listItemView.findViewById(R.id.textViewPrimary);
         TextView secondaryItem = (TextView) listItemView.findViewById(R.id.textViewSecondary);
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
 
         primaryItem.setText(currentWord.getMiwokTranslation());
         secondaryItem.setText(currentWord.getDefaultTranslation());
+        imageView.setImageResource(currentWord.getImageResourceId());
 
         return listItemView;
     }
